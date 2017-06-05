623357dbf3282da793a4de0360d3b85f07ad98ba
Geraint Edwards <geraint.edwards@ons.gov.uk>
2017-06-05 13:55:44 +0100

    change ci build script to copy additional files (static/ dir)

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,5 +5,5 @@ cwd=$(pwd)
 export GOPATH=$cwd/go
 
 pushd $GOPATH/src/github.com/ONSdigital/dp-content-api
-  make build && mv build/$(go env GOOS)-$(go env GOARCH)/bin/* $cwd/build
+  make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
 popd
